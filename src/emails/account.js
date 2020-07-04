735f6fc52b378f74e01cce505d4fe5a57ec501f4
HUSSEIN AMR AHMAD MAGED ABDALAZIM BADAWY <husseinbadawy@std.sehir.edu.tr>
2020-07-04 15:26:57 +0300

    edited text

--- src/emails/account.js
+++ src/emails/account.js
@@ -8,7 +8,7 @@ const sendWelcomeEmail = (email, name) => {
         to: email,
         from: 'husseinbadawy@std.sehir.edu.tr',
         subject: `Welcome,  ${name}!`,
-        text: `We are very happy to have you, ${name} on our side and have you use our FANTASTIC servisnations!`
+        text: `We are very happy to have you, ${name}, using our application!`
     })
 }
 
@@ -17,9 +17,7 @@ const sendCancelationEmail = (email, name) => {
         to: email,
         from: 'husseinbadawy@std.sehir.edu.tr',
         subject: `Why did you cancel, borther  ${name}..`,
-        text: `We are very sad to see you, ${name}, leave
-        behind our FANTASTIC servisnations! can you tell me why you left please
-        you piece of **** so i can do better with my other clients? Thank you bro.`
+        text: `We are very sad to see you leave ${name}, we really hope to see you again soon.`
     })
 }
 
